ac1a15273048a77c63007646507eb98c590a76eb
steve <austines811@gmail.com>
2023-05-21 11:08:29 +0300

    starts flask and run app

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,8 @@
 """
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
 
 if __name__ == '__main__':
     app = Flask(__name__)
@@ -47,5 +49,25 @@ if __name__ == '__main__':
         """Display a HTML page only if n is an integer
         """
         return render_template('5-number.html', n=n)
+
+    @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+    def number_odd_or_even(n):
+        """Display a HTML page only if n is an integer
+        """
+        parity = 'even' if n % 2 == 0 else 'odd'
+        return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+    @app.route('/states_list', strict_slashes=False)
+    def states_list():
+        """Display a HTML page of the States
+        """
+        states = storage.all(State).values()
+        return render_template('7-states_list.html', states=states)
+
+    @app.teardown_appcontext
+    def teardown_db(error):
+        """Closes the database again at the end of the request.
+        """
+        storage.close()
 
     app.run('0.0.0.0')
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,8 @@
 """
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
 
 if __name__ == '__main__':
     app = Flask(__name__)
@@ -47,5 +49,40 @@ if __name__ == '__main__':
         """Display a HTML page only if n is an integer
         """
         return render_template('5-number.html', n=n)
+
+    @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+    def number_odd_or_even(n):
+        """Display a HTML page only if n is an integer
+        """
+        parity = 'even' if n % 2 == 0 else 'odd'
+        return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+    @app.route('/states_list', strict_slashes=False)
+    def states_list():
+        """Display a HTML page of the States
+        """
+        states = storage.all(State).values()
+        return render_template('7-states_list.html', states=states)
+
+    @app.route('/cities_by_states', strict_slashes=False)
+    def cities_by_states():
+        """Display a HTML page of the States and the
+        Cities by State
+        """
+        states = storage.all(State).values()
+        cities = list()
+
+        for state in states:
+            for city in state.cities:
+                cities.append(city)
+
+        return render_template('8-cities_by_states.html',
+                               states=states, state_cities=cities)
+
+    @app.teardown_appcontext
+    def teardown_db(error):
+        """Closes the database again at the end of the request.
+        """
+        storage.close()
 
     app.run('0.0.0.0')
